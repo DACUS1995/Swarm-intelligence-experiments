904a58725db2a8ab73d374ad2b0e32074795ae56
DACUS1995 <studormarian@gmail.com>
2019-11-01 23:12:20 +0200

    added benchmark functions

--- Particle Swarm Optimization Experiment/cost_functions.py
+++ Particle Swarm Optimization Experiment/cost_functions.py
@@ -1,3 +1,5 @@
+import math
+
 def spherical_fn(x):
 	total = 0
 
@@ -9,19 +11,19 @@ def rastrigin_fn(x):
 	total = 0
 
 	for i in range(len(x)):
-		total += x[i]**2
+		total += ( x[i]**2 - 10 * (math.cos(2 * math.pi * x[i])) + 10 )
 	return total
 
 def griewank_fn(x):
 	total = 0
 
 	for i in range(len(x)):
-		total += x[i]**2
+		total += ( (x[i]**2 / 4000) + math.cos(x[i] / math.sqrt(i + 1) + 1) )
 	return total
 
 def rosenbrock_fn(x):
 	total = 0
 
-	for i in range(len(x)):
-		total += x[i]**2
+	for i in range(len(x) - 1):
+		total += ( 100 * ((x[i] - x[i]**2)**2) + (x[i] - 1)**2 )
 	return total
